4800753dee9c586202ad0ace7e691acd7bd55dbc
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-08-03 15:47:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LifeStyle extends Component {
+  constructor(){
+    super();
+    this.state = {
+      items: [],
+      isloaded: false,
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({
+          isloaded: true,
+          items: data,
+        })
+      })
+  }
+  render(){
+    if(!this.state.isloaded){
+      return <div>Loading...</div>
+    }
+    return(
+      <div>
+        <ul>
+          {this.state.items.map((ele)=><li>Name: {ele.name} | UserName: {ele.username} | <a href={'http://'+ ele.website} target='blank'>Website</a></li>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default LifeStyle;
